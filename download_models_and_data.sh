415474eb2b7f707ae5e5a5a5c43da289b7cd3878
a46068902 <a46068902@gmail.com>
2022-11-18 12:51:27 -0500

    Initial commit

---
+++ download_models_and_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gdown https://drive.google.com/uc?id=1x6RYJg3UcdwY6jqCle8yIFYdmX6hKlmF && mv TartanAir.pickle datasets/
+gdown https://drive.google.com/uc?id=1peMdLeCYK-RXr8gYe8LiZAX8XBMSPl3D && unzip models.zip
+gdown https://drive.google.com/uc?id=1SnUvWhOYjPi_tOsuN90nVYPxUFTGVkDO && unzip movies.zip
